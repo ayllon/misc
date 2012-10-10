dc92806cf89cd5d119ae71da8932b699ecc63f87
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2012-10-10 11:53:45 +0200

    Wrong file :S

--- int-encode/encode.c
+++ int-encode/encode.c
@@ -1,4 +1,7 @@
+#include <assert.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 
@@ -53,11 +56,9 @@ size_t decode(const unsigned char *data, size_t nbytes,
   return outp;
 }
 
-
-
 static uint8_t base64_digit(uint8_t value)
 {
-  if (value < 26)       return 'A' + value;
+  if      (value < 26)  return 'A' + value;
   else if (value < 52)  return 'a' + value - 26;
   else if (value < 62)  return '0' + value - 52;
   else if (value == 62) return '+';
@@ -65,8 +66,7 @@ static uint8_t base64_digit(uint8_t value)
   else                  abort(); 
 }
 
-
-int base64_encode(const unsigned char *in, size_t inbytes, unsigned char *out, size_t outsize)
+size_t base64_encode(const unsigned char *in, size_t inbytes, unsigned char *out, size_t outsize)
 {
   size_t ii, oi, padding;
  
@@ -91,9 +91,58 @@ int base64_encode(const unsigned char *in, size_t inbytes, unsigned char *out, s
     out[oi - 1 - ii] = '=';
   }
 
-  out[oi++] = '\0';
+  if (oi < outsize) out[oi++] = '\0';
   return oi; 
 }
+
+static uint8_t base64_value(uint8_t digit)
+{
+  errno = 0;
+  if      (digit >= 'A' && digit <= 'Z') return digit - 'A';
+  else if (digit >= 'a' && digit <= 'z') return digit - 'a' + 26;
+  else if (digit >= '0' && digit <= '9') return digit - '0' + 52;
+  else if (digit == '+')                 return 62;
+  else if (digit == '/')                 return 63;
+  else {
+    errno = EINVAL;
+    return 0;
+  }
+}
+
+size_t base64_decode(const unsigned char *in, unsigned char *out, size_t outsize)
+{
+  size_t ii, oi;
+  
+  for (ii = 0, oi = 0; in[ii] != '\0' && oi < outsize; ) {
+    uint8_t  digit;
+    uint32_t triplet;
+
+    triplet = 0;
+    do {
+      digit   = in[ii++];
+      if (digit == '=') break;
+      triplet = (triplet << 6) | base64_value(digit);
+    } while (in[ii] != '\0' && ii % 4);
+
+    if (digit == '=' && in[ii] == '=') {
+       out[oi++] = (triplet >> 4) & 0xFF;
+       break;
+    }
+    else if (digit == '=') {
+      out[oi++] = (triplet >> 10) & 0xFF;
+      out[oi++] = (triplet >>  2) & 0xFF;
+      break;
+    }
+    else {
+      out[oi++] = (triplet >> 16) & 0xFF;
+      out[oi++] = (triplet >>  8) & 0xFF;
+      out[oi++] = (triplet      ) & 0xFF;
+    }
+  }
+
+  return oi;
+}
+
 
 #define ARRAY_LEN 10
 
@@ -101,26 +150,25 @@ int main(int argc, char *argv[])
 {
   long          array[ARRAY_LEN];
   long          validate[ARRAY_LEN];
-  unsigned char buffer[512], base64[1024];
+  unsigned char buffer[512], base64[1024], buffer2[512];
   size_t        i, n, n2;
 
-  srandom(time(NULL));
+  if (argc < 2) {
+    printf("Using time as seed.\n");
+    srandom(time(NULL));
+  }
+  else {
+    int seed = atoi(argv[1]);
+    printf("Using %d as seed.\n", seed);
+    srandom(seed);
+  }
+
   for (i = 0; i < ARRAY_LEN; ++i)
     array[i] = random();
 
   n = encode(array, ARRAY_LEN,
              buffer, sizeof(buffer));
 
-  for (i = 0; i < n; ++i) {
-    if (i % 8 == 0)
-      printf("%02lu => %02lu\t", i, i + 7);
-    
-    printf("0x%02X ", buffer[i]);
-    
-    if (i % 8 == 7)
-      printf("\n");
-  }
-  printf("\n");
   printf("Space used: %lu (original %lu)\n", n, sizeof(array));
 
   /* Validate */
@@ -136,12 +184,39 @@ int main(int argc, char *argv[])
       return 1;
     }
   }
-  printf("All OK :)\n");
+  printf("Encode/Decode OK\n");
 
   /* Base 64 */
   n2 = base64_encode(buffer, n, base64, sizeof(base64)); 
-  base64[n2] = '\0';
-  printf("Base 64 encoding (%ld characters): %s\n", n2, base64);
+  if (n2 > 0) {
+    base64[n2] = '\0';
+    printf("Base 64 encoding (%ld characters): %s\n", n2, base64);
+  }
+  else {
+    printf("Buffer size too small\n");
+    return 1;
+  }
+
+  /* Digits */
+  for (i = 0; i < 64; ++i) {
+    assert(base64_value(base64_digit(i)) == i);
+  }
+
+  /* Decode */
+  n2 = base64_decode(base64, buffer2, sizeof(buffer2));
+  if (n2 != n) {
+    printf("Expected %d bytes, got %d\n", n, n2);
+    return 1;
+  }
+
+  for (i = 0; i < n2; ++i) {
+    if (buffer2[i] != buffer[i]) {
+      printf("Position %lu: expected %u, got %u\n", i, buffer[i], buffer2[i]);
+      return 1;
+    }
+  }
+
+  printf("Base64 encode/decode OK\n");
 
   return 0;
 }
